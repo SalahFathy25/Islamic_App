9886eccb6cf5258d4913d3058608bf642bd0641f
Salah Fathy <salah.fathy@cgtech.com.sa>
2025-11-05 13:32:49 +0200

    - enable logger in bloc observer

--- lib/app/core/routes/bloc_observer.dart
+++ lib/app/core/routes/bloc_observer.dart
@@ -1,48 +1,56 @@
-import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logger/logger.dart';
 
-
-// import 'package:logger/logger.dart';
-
-//  Logger  logger = Logger();
+final logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 1,
+    // عدد الأسطر في الـ stacktrace
+    errorMethodCount: 5,
+    lineLength: 100,
+    colors: true,
+    printEmojis: true,
+    printTime: false,
+  ),
+);
 
 class MyBlocObserver extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-
-    //  logger.d('onCreate -- ${bloc.runtimeType}');
-    log('onCreate -- ${bloc.runtimeType}');
+    logger.i('🟢 onCreate -- ${bloc.runtimeType}');
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    log('onEvent -- ${bloc.runtimeType}, $event');
+    logger.v('📩 onEvent -- ${bloc.runtimeType}, event: $event');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    log('onChange -- ${bloc.runtimeType}, $change');
+    logger.d('🔄 onChange -- ${bloc.runtimeType}, change: $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    log('onTransition -- ${bloc.runtimeType}, $transition');
+    logger.d('➡️ onTransition -- ${bloc.runtimeType}, transition: $transition');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    log('onError -- ${bloc.runtimeType}, $error');
+    logger.e(
+      '❌ onError -- ${bloc.runtimeType}, error: $error',
+      error: error,
+      stackTrace: stackTrace,
+    );
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onClose(BlocBase bloc) {
     super.onClose(bloc);
-    log('onClose -- ${bloc.runtimeType}');
+    logger.w('🔴 onClose -- ${bloc.runtimeType}');
   }
 }
