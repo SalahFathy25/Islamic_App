6902e20b7e6dc1538a08eb62339942ec49003bd7
Salah Fathy <salah.fathy@cgtech.com.sa>
2025-11-08 12:33:46 +0200

    - change font family for app

--- lib/app/core/utils/font_style.dart
+++ lib/app/core/utils/font_style.dart
@@ -1,79 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:islamic_app/generated/assets.dart';
 
 import 'app_colors.dart';
 
 class AppFontStyle {
   static TextStyle fontAlmarai10w400White = TextStyle(
     fontSize: 10.sp,
-    fontFamily: Assets.fontsAlmaraiRegular,
+    fontFamily: 'Almarai',
     fontWeight: FontWeight.w400,
     color: AppColors.white,
   );
   static TextStyle fontAlmarai12w400mainColor = TextStyle(
     fontSize: 12.sp,
-    fontFamily: Assets.fontsAlmaraiRegular,
+    fontFamily: 'Almarai',
     fontWeight: FontWeight.w400,
     color: AppColors.mainColor,
   );
   static TextStyle fontAlmarai14w700White = TextStyle(
     fontSize: 14.sp,
-    fontFamily: Assets.fontsAlmaraiBold,
+    fontFamily: 'Almarai',
     fontWeight: FontWeight.w700,
     color: AppColors.white,
   );
   static TextStyle fontAlmarai14w400White = TextStyle(
     fontSize: 14.sp,
-    fontFamily: Assets.fontsAlmaraiRegular,
+    fontFamily: 'Almarai',
     fontWeight: FontWeight.w400,
     color: AppColors.white,
   );
   static TextStyle fontAlmarai14w800White = TextStyle(
     fontSize: 14.sp,
-    fontFamily: Assets.fontsAlmaraiExtraBold,
+    fontFamily: 'Almarai',
     fontWeight: FontWeight.w800,
     color: AppColors.white,
   );
   static TextStyle fontAlmarai14w800Black = TextStyle(
     fontSize: 14.sp,
-    fontFamily: Assets.fontsAlmaraiExtraBold,
+    fontFamily: 'Almarai',
     fontWeight: FontWeight.w800,
     color: AppColors.black,
   );
   static TextStyle fontCairo14w400pink = TextStyle(
     fontSize: 14.sp,
-    fontFamily: Assets.fontsCairoRegular,
+    fontFamily: 'Cairo',
     fontWeight: FontWeight.w400,
     color: AppColors.pinkColor,
   );
   static TextStyle fontAmiri16w400titleColor = TextStyle(
     fontSize: 16.sp,
-    fontFamily: Assets.fontsAmiriQuranRegular,
+    fontFamily: 'AmiriQuran',
     fontWeight: FontWeight.w400,
     color: AppColors.mainColor,
   );
   static TextStyle fontCairo18w700black = TextStyle(
     fontSize: 18.sp,
-    fontFamily: Assets.fontsReemKufiFunBold,
+    fontFamily: 'Cairo',
     fontWeight: FontWeight.w700,
     color: AppColors.black,
   );
   static TextStyle fontReemKafi18w700titleColor = TextStyle(
     fontSize: 18.sp,
-    fontFamily: Assets.fontsReemKufiFunBold,
+    fontFamily: 'ReemKufi',
     fontWeight: FontWeight.w700,
     color: AppColors.mainColor,
   );
   static TextStyle fontReemKafi20w600titleColor = TextStyle(
     fontSize: 20.sp,
-    fontFamily: Assets.fontsReemKufiFunBold,
+    fontFamily: 'ReemKufi',
     fontWeight: FontWeight.w600,
     color: AppColors.mainColor.withValues(alpha: 0.9),
   );
   static TextStyle fontReemKafi20w400titleColor = TextStyle(
     fontSize: 20.sp,
-    fontFamily: Assets.fontsReemKufiFunRegular,
+    fontFamily: 'ReemKufi',
     fontWeight: FontWeight.w400,
     color: AppColors.mainColor.withValues(alpha: 0.9),
   );
