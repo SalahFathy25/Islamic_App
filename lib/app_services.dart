47d0caf59c3f8659b5cd04ca133882f8707a88dd
Salah Fathy <salah.fathy@cgtech.com.sa>
2025-11-12 21:20:39 +0200

    initialize firebase in app

--- lib/app_services.dart
+++ lib/app_services.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app/core/configurations/shared_preferences.dart';
 import 'app/core/routes/bloc_observer.dart';
+import 'firebase_options.dart';
 
 void appServices() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
   Bloc.observer = MyBlocObserver();
   await CacheHelper.initCacheHelper();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
